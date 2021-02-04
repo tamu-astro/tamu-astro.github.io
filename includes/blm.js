fef364d8871ff61808544436e94251ce05853870
Jonathan Cohn <jonathancohn@connect-172-31-29-17.vpn.tamu.edu>
2021-02-04 14:54:05 -0600

    making blm and land acknowledgement all the same everywhere

---
+++ includes/blm.js
@@ -0,0 +1,5 @@
+document.write("<header class='major'>");
+document.write("<h2>Statement Against White Supremacy: <font color='#B9A44E'>Black Lives Matter</font></h2></header>");
+document.write("<p>White supremacy pervades our society, systematically and intentionally oppressing Black people. The United States of America is crafted to burden Black people with poverty and neglect their health. Furthermore, it sustains legal slavery via an unjust prison system that disproportionately incarcerates Black people. Additionally, police officers regularly use excessive force and often murder Black people with no legal consequences.</p>");
+
+document.write("<p>We demand justice for Breonna Taylor, George Floyd, Ahmaud Arbery, Daniel Prude, Nina Pop, Rayshard Brooks, David McAtee, Dijon Kizzee, Casey Goodson, and all those who suffer and have suffered at the hands of police brutality and systemic racism. Black lives matter.</br></br></p>");
---
+++ includes/land.js
@@ -0,0 +1,13 @@
+document.write("<header class='major'>");
+document.write("<h2>Indigenous Land Acknowledgement</h2></header>");
+document.write("<p><strong>The George P. &amp; Cynthia Woods Mitchell Institute for Fundamental Physics and Astronomy</strong>, and all of Texas A&amp;M University, <strong>sits on unceded native land.</strong>   <!-- Tonkawa --> We want to acknowledge the aboriginal and indigenous peoples’ land which we occupy. This includes the aboriginal homeland of the Tonkawa people. The Texas state government forcibly removed the Tonkawa in 1855, ultimately relocating them to their current reservation in northern Oklahoma. <!-- Bidai --> The Brazos County region was also the traditional land of the Atakapa-Ishak people, particularly the Bidai. About half the Bidai were killed in an epidemic in the 1770s, after contact with European colonizers, and the Texas State government forcibly removed the few remaining Bidai in 1854.</p>");
+document.write("<h3>Resources:</h3>");
+document.write("<ul><ul>");
+document.write("<li>Native Land Digital, Indigenous-led not-for-profit (<a href='https://native-land.ca/' target='_blank'>website</a>)</li>");
+document.write("<li>The Tonkawa Tribe (<a href='http://tonkawatribe.com/' target='_blank'>website</a>)</li>")
+document.write("<li>The Tonkawa (Wikipedia, <a href='https://en.wikipedia.org/wiki/Tonkawa' target='_blank'>website</a>)</li>");
+document.write("<li>The Atakapa-Ishak Nation (<a href='http://www.atakapa-ishak.org/history/' target='blank'>website</a>)</li>");
+document.write("<li>The Bidai in Texas (<a href='https://www.tshaonline.org/handbook/entries/bidai-indians' target='_blank'>website</a>)</li>");
+document.write("<li>The Bidai in Brazos County (<a href='http://www.texascenterforregionalstudies.net/the-bidai-tribe-of-intrigue-who-were-they-really.html' target='_blank'>website</a>)</li>");
+document.write("<li>The Bidai (Wikipedia, <a href='https://en.wikipedia.org/wiki/Bidai' target='_blank'>website</a>)</li>");
+document.write("</ul></ul> <!-- doubled <ul> to make it nicer -->");
