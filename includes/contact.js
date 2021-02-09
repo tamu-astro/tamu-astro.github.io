3fa4245ac025bba03afdfbfc261ac683f476d86f
Taylor Hutchison <aibhleog@tamu.edu>
2021-02-09 12:46:34 -0600

    uniformalizing footer content :weary:

---
+++ includes/contact.js
@@ -0,0 +1,9 @@
+document.write("<header class='major'>");
+document.write("<h2>Get in touch</h2>");
+document.write("</header>");
+document.write("<p>If you have any questions, please feel free to reach out to us!</p>");
+document.write("<ul class='contact'>");
+document.write("<li class='icon solid fa-envelope'>Graduate Student Representative: Taylor Hutchison, <a href='mailto:aibhleog@tamu.edu'>aibhleog@tamu.edu</a></li>");
+document.write("<li class='icon solid fa-phone'> TAMU Physics &amp; Astronomy Dept. #: (979) 845-7717</li>");
+document.write("<li class='icon solid fa-home'>Dept of Physics &amp; Astronomy </br> 4242 TAMU </br> College Station, TX </br> 77843-4242</li>");
+document.write("</ul>");
