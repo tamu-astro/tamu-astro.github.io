641b770978826899cb2b4c101d13459625bf1102
Jonathan Cohn <jonathancohn@Jonathans-MacBook-Pro-2.local>
2021-03-25 14:02:29 -0500

    update

--- includes/blm.js
+++ includes/blm.js
@@ -1,5 +1,8 @@
 document.write("<header class='major'>");
-document.write("<h2>Statement Against White Supremacy: <font color='#B9A44E'>Black Lives Matter</font></h2></header>");
+document.write("<h2>Statement Against White Supremacy</h2></header>");
+document.write("<font color='#B9A44E'><h2>Stop Asian and NHPI Hate</font></h2>");
+document.write("<p>We condemn acts of violence against Asian, Asian American, Native Hawai’ian, and Pacific Islander communities, which has escalated with increasingly widespread racism and xenophobia during the COVID-19 pandemic. We stand in solidarity with Asian, Asian American, Native Hawaiian, and Pacific Islander communities, and demand justice for all those who suffer at the hands of white supremacy.</p>")
+document.write("<font color='#B9A44E'><h2>Black Lives Matter</font></h2>");
 document.write("<p>White supremacy pervades our society, systematically and intentionally oppressing Black people. The United States of America is crafted to burden Black people with poverty and neglect their health. Furthermore, it sustains legal slavery via an unjust prison system that disproportionately incarcerates Black people. Additionally, police officers regularly use excessive force and often murder Black people with no legal consequences.</p>");
 
 document.write("<p>We demand justice for Breonna Taylor, George Floyd, Ahmaud Arbery, Daniel Prude, Nina Pop, Rayshard Brooks, David McAtee, Dijon Kizzee, Casey Goodson, and all those who suffer and have suffered at the hands of police brutality and systemic racism. Black lives matter.</br></br></p>");
