1aa73e9a92ef7f7bb2018c18c585eed61788f713
Jonathan Cohn <jonathancohn@Jonathans-MacBook-Pro-2.local>
2021-04-26 16:13:16 -0500

    update from Grisha

--- includes/disclaimer.js
+++ includes/disclaimer.js
@@ -1,5 +1,5 @@
 document.write("<section>");
 document.write("<p>The contents of this website reflect our best efforts at summarizing some of the policies and regulations associated with the Astronomy graduate program at Texas A&amp;M University. They also reflect the consensus personal opinions of members of the Astronomy group at Texas A&amp;M University.</p>");
-document.write("<p>Under no circumstances should any of the content of this website be considered the official view or policy of Texas A&amp;M University or the Texas A&amp;M System</p>");
+document.write("<p>Under no circumstances should any of the content of this website be considered the official view or policy of Texas A&amp;M University, the Texas A&amp;M Department of Physics & Astronomy, the College of Science, or the Texas A&amp;M System.</p>");
 document.write("<p class='copyright'>&copy; TAMU Astro grad students. All rights reserved. Design: <a href='https://html5up.net'>HTML5 UP</a>.</p>");
 document.write("</section>");
