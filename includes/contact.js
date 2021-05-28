92435d7ad5d6675e865d93c0b01f884f761f5b85
Taylor Hutchison <aibhleog@tamu.edu>
2021-05-28 17:15:07 -0500

    updating grad rep contact :tada:

--- includes/contact.js
+++ includes/contact.js
@@ -3,7 +3,7 @@ document.write("<h2>Get in touch</h2>");
 document.write("</header>");
 document.write("<p>If you have any questions, please feel free to reach out to us!</p>");
 document.write("<ul class='contact'>");
-document.write("<li class='icon solid fa-envelope'>Graduate Student Representative: Taylor Hutchison, <a href='mailto:aibhleog@tamu.edu'>aibhleog@tamu.edu</a></li>");
+document.write("<li class='icon solid fa-envelope'>Graduate Student Representative: Jonathan Cohn, <a href='mailto:joncohn@tamu.edu'>joncohn@tamu.edu</a></li>");
 document.write("<li class='icon solid fa-envelope'>Postdocs &amp; Researchers contact: Terese Hansen, <a href='mailto:thidemannhansen@gmail.com'>thidemannhansen@gmail.com</a></li>");
 document.write("<li class='icon solid fa-envelope'>Faculty contact &amp; Astronomy graduate program faculty advisor: Lucas Macri, <a href='mailto:lmacri@tamu.edu'>lmacri@tamu.edu</a></li>");
 document.write("</ul>");
