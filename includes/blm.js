9fd265189b3bfe860a0f3ca73fad70a3e90bc564
Taylor Hutchison <aibhleog@tamu.edu>
2021-05-29 14:53:00 -0500

    removing ' from Hawaiian

--- includes/blm.js
+++ includes/blm.js
@@ -1,7 +1,7 @@
 document.write("<header class='major'>");
 document.write("<h2>Statement Against White Supremacy</h2></header>");
 document.write("<font color='#B9A44E'><h2>Stop Asian and NHPI Hate</font></h2>");
-document.write("<p>We condemn acts of violence against Asian, Asian American, Native Hawai’ian, and Pacific Islander communities, which have escalated with increasingly widespread racism and xenophobia during the COVID-19 pandemic. We stand in solidarity with Asian, Asian American, Native Hawaiian, and Pacific Islander communities and demand justice for all those who suffer at the hands of white supremacy.</p>")
+document.write("<p>We condemn acts of violence against Asian, Asian American, Native Hawaiian, and Pacific Islander communities, which have escalated with increasingly widespread racism and xenophobia during the COVID-19 pandemic. We stand in solidarity with Asian, Asian American, Native Hawaiian, and Pacific Islander communities and demand justice for all those who suffer at the hands of white supremacy.</p>")
 document.write("<font color='#B9A44E'><h2>Black Lives Matter</font></h2>");
 document.write("<p>White supremacy pervades our society, systematically and intentionally oppressing Black people. The United States of America is crafted to burden Black people with poverty and neglect their health. Furthermore, it sustains legal slavery via an unjust prison system that disproportionately incarcerates Black people. Additionally, police officers regularly use excessive force and often murder Black people with no legal consequences.</p>");
 
