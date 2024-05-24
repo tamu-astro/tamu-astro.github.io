af05c55a4ebc7d881ee20b39b34b8bbdd96e4e14
Nikko Cleri <61384356+njcleri@users.noreply.github.com>
2024-05-24 09:31:23 -0500

    Update contact.js
    updated kaitlin

--- includes/contact.js
+++ includes/contact.js
@@ -3,7 +3,7 @@ document.write("<h2>Get in touch</h2>");
 document.write("</header>");
 document.write("<p>If you have any questions, please feel free to reach out to us!</p>");
 document.write("<ul class='contact'>");
-document.write("<li class='icon solid fa-envelope'>Graduate Student Representative: Nikko Cleri, <a href='mailto:cleri@tamu.edu'>cleri@tamu.edu</a></li>");
+document.write("<li class='icon solid fa-envelope'>Graduate Student Representative: Kaitlin Webber, <a href='mailto:kbwebber@tamu.edu'>kbwebber@tamu.edu</a></li>");
 document.write("<li class='icon solid fa-envelope'>Postdocs &amp; Researchers contact: Jack Birkin, <a href='mailto:jbirkin@tamu.edu'>jbirkin@tamu.edu</a></li>");
 // document.write("<li class='icon solid fa-envelope'>Faculty contact &amp; Astronomy graduate program faculty advisor: Lucas Macri, <a href='mailto:lmacri@tamu.edu'>lmacri@tamu.edu</a></li>");
 document.write("</ul>");
